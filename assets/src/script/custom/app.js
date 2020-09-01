8f7c423ebc1e3cfb0db02fd413ecea6ef6511145
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-09-01 12:40:05 +0300

    Project Started

---
+++ assets/src/script/custom/app.js
@@ -0,0 +1,26 @@
+
+/* Custom Owl Class
+$('.customowlclass').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    autoWidth:false,
+    items:5,
+    margin:0,
+    mouseDrag:false,
+    animateIn: 'fadeInUp',
+    animateOut: 'fadeOutUp',
+    responsiveClass:true,
+    responsive:{
+        0:{},
+        819:{},
+        1059:{},
+        1379:{},
+        1619:{},
+        1939:{}
+    }
+});
+/* Custom Owl Class */
