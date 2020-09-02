00d92103e40464df468db2ca96ebad1ad114eef2
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-09-02 16:59:24 +0300

    Homepage Working

--- assets/src/script/custom/app.js
+++ assets/src/script/custom/app.js
@@ -1,19 +1,16 @@
 
-
 /* Head Home Slider */
 $('.headsliderowl').owlCarousel({
     loop: true,
     nav: false,
     dots: false,
     autoplay: true,
-    autoplayTimeout: 10000,
+    autoplayTimeout: 5000,
     autoplayHoverPause: false,
     items: 1,
     margin: 0,
     autoWidth: false,
     mouseDrag: true,
-    animateIn: 'fadeInUp',
-    animateOut: 'fadeOutUp',
     responsiveClass: true,
     responsive:{
         0:{},
@@ -25,3 +22,40 @@ $('.headsliderowl').owlCarousel({
     }
 });
 /* Head Home Slider */
+
+/* Home Procedures */
+$(document).ready(function() {
+    var proceduresitem = document.querySelectorAll(".proceduresimg");
+    for (var i = 0; i < proceduresitem.length; i++) {
+        proceduresitem[i].style.backgroundImage     = "url('" + proceduresitem[i].getAttribute('data-img-url') + "')";
+        proceduresitem[i].style.backgroundSize      = proceduresitem[i].getAttribute('data-img-size');
+        proceduresitem[i].style.backgroundPositionY = proceduresitem[i].getAttribute('data-position-y');
+        proceduresitem[i].style.backgroundPositionX = proceduresitem[i].getAttribute('data-position-x');
+    }
+});
+
+$('.homeproceduresowl').owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    autoplayHoverPause: false,
+    items: 5,
+    margin: 0,
+    autoWidth: false,
+    mouseDrag: true,
+    animateIn: 'fadeInUp',
+    animateOut: 'fadeOutUp',
+    center: true,
+    responsiveClass: true,
+    responsive:{
+        0:{},
+        819:{},
+        1059:{},
+        1379:{},
+        1619:{},
+        1939:{}
+    }
+});
+/* Home Procedures */
