50be0af970a3aa08d05cc9ea54468db4f62dca4f
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-09-04 15:03:48 +0300

    Header Navigation Menu Added

--- assets/src/script/plugin/app.js
+++ assets/src/script/plugin/app.js
@@ -20,3 +20,17 @@ jQuery(function($) {
     }
 });
 /* Header Web Navbar Fixed */
+
+/* Header Navigation Navbar */
+$('.headnavigation-navmenu .dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+        $('.dropdown-submenu .show').removeClass("show");
+    });
+    return false;
+});
+/* Header Navigation Navbar */
